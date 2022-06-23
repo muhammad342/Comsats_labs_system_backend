96819d3dab5a6e81d1e55237a2a1b81bc0ab48ce
muhammad342 <muhammadashraf342@yahoo.com>
2022-06-24 00:06:52 +0500

    fix

--- Routes/labRoutes.js
+++ Routes/labRoutes.js
@@ -4,10 +4,8 @@ const router = express.Router();
 const labController = require("../controller/labController");
 const authMiddleware = require("../middleware/authMiddleware");
 
-router.route("/").post( labController.AddLab).get(labController.AllLab);
-router.route("/allLab").get( labController.AllLab).get(labController.AllLab);
-
+router.route("/").post(labController.AddLab).get(labController.AllLab);
+router.route("/allLab").get(labController.AllLab).get(labController.AllLab);
+router.route("/updateLab").put(labController.UpdateLab);
 const labRoutes = router;
 module.exports = labRoutes;
-
-
--- controller/labController.js
+++ controller/labController.js
@@ -7,40 +7,50 @@ const generateToken = require("../utils/generateToken");
 // @access  Public
 
 const AddLab = asyncHandler(async (req, res) => {
-  const { name, softwares} = req.body;
-  const createdLab = await Lab.create({name,softwares})
-  if(createdLab)
-  {
+  const { name, softwares } = req.body;
+  const createdLab = await Lab.create({ name, softwares });
+  if (createdLab) {
     res.json({
-        message: "Successfully Created",
-        data: createdLab,
-        success: true,
-    })
-
-  }
-  else {
-      throw new Error("Lab not created");
+      message: "Successfully Created",
+      data: createdLab,
+      success: true,
+    });
+  } else {
+    throw new Error("Lab not created");
   }
 });
 
+const UpdateLab = asyncHandler(async (req, res) => {
+  const { _id, softwares } = req.body;
+  console.log(req.body._id);
+  const lab = await Lab.findOne({ _id });
+  if (lab) {
+    const software = lab.softwares;
+    lab.softwares = [...software, softwares];
+
+    const updatedLab = await lab.save();
+
+    res.json({
+      _id: updatedLab._id,
+      name: updatedLab.name,
+      softwares: updatedLab.softwares,
+    });
+  } else {
+    throw new Error("Lab not Found");
+  }
+});
 
 const AllLab = asyncHandler(async (req, res) => {
-    const allLabs = await Lab.find({});
-    if(allLabs)
-    {
-      res.json(
-         
-           allLabs,
-      
-      )
-    }
-    else {
-        throw new Error("Lab not created");
-    }
-    
-  });
+  const allLabs = await Lab.find({});
+  if (allLabs) {
+    res.json(allLabs);
+  } else {
+    throw new Error("Lab not created");
+  }
+});
 
 module.exports = {
-    AddLab,
-    AllLab
+  AddLab,
+  AllLab,
+  UpdateLab,
 };
